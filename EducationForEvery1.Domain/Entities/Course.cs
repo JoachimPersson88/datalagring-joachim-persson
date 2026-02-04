5bc858bd73d651a212616eedeae5542199907c18
JoachimPersson88 <joachim.persson88@gmail.com>
2026-02-04 22:50:37 +0100

    "Domain" Skapat Course-fil, där jag har skapat get struktur för Id, title och description, sen har jag även skapat throw exception

---
+++ EducationForEvery1.Domain/Entities/Course.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationForEvery1.Domain.Entities
+{
+    public class Course
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; } = string.Empty;
+        public string? Description { get; private set; }
+
+        private Course() { }
+
+        public Course(string title, string? description)
+        {
+            SetTitle(title);
+            Description = description;
+        }
+
+        public void SetTitle(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException("Title cannot be empty.");
+
+            Title = title.Trim();
+        }
+
+        public void SetDescription(string? description) => Description = description;
+
+    }
+}
