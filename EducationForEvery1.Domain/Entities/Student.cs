faeffad4adfbdf81f06c0647fba6fc64df9d55d3
JoachimPersson88 <joachim.persson88@gmail.com>
2026-02-12 18:35:05 +0100

    "Domain_Entities" Skapade Student klass med namn och mail-adress

---
+++ EducationForEvery1.Domain/Entities/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationForEvery1.Domain.Entities;
+public class Student
+{
+    public int Id { get; private set; }
+
+    public string FirstName { get; private set; } = string.Empty;
+    public string LastName { get; private set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+
+    private Student() { }
+
+    public Student(string firstName, string lastName, string email)
+    {
+        SetName(firstName, lastName);
+        SetEmail(email);
+    }
+
+    public void SetName(string firstName, string lastName)
+    {
+        if (string.IsNullOrWhiteSpace(firstName)) throw new ArgumentException("FirstName is required.");
+        if (string.IsNullOrWhiteSpace(lastName)) throw new ArgumentException("LastName is required.");
+
+        FirstName = firstName.Trim();
+        LastName = lastName.Trim();
+    }
+
+    public void SetEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email)) throw new ArgumentException("Email is required.");
+        Email = email.Trim().ToLowerInvariant();
+    }
+}
