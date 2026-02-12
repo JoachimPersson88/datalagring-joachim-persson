1ecefe91141c0cef94d96c1c200f8c57a252692b
JoachimPersson88 <joachim.persson88@gmail.com>
2026-02-12 19:42:32 +0100

    "Domain_Entities" La till navigationslista för enrollment

--- EducationForEvery1.Domain/Entities/Enrollment.cs
+++ EducationForEvery1.Domain/Entities/Enrollment.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace EducationForEvery1.Domain.Entities
-    ;
+namespace EducationForEvery1.Domain.Entities;
+
 public enum EnrollmentStatus
 {
     Active = 1,
@@ -32,6 +32,14 @@ public class Enrollment
         EnrolledAtUtc = DateTime.UtcNow;
         Status = EnrollmentStatus.Active;
     }
+
+    private readonly List<Enrollment> _enrollments = new();
+
+    public IReadOnlyCollection<Enrollment> Enrollments => _enrollments;
+
+    private readonly List<CourseInstanceTeacher> _teachers = new();
+
+    public IReadOnlyCollection<CourseInstanceTeacher> Teachers => _teachers;
 
     public void Cancel() => Status = EnrollmentStatus.Cancelled;
 }
