0d9956b05c9e0dfc1726dfda817cac315d987880
JoachimPersson88 <joachim.persson88@gmail.com>
2026-02-12 19:18:20 +0100

    "Domain_Entities" Skapat inskrivnings klass som innehåller studentID, kursID, datum och status. Jag skapade även enum för klassen som håller koll på statusen.

---
+++ EducationForEvery1.Domain/Entities/Enrollment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationForEvery1.Domain.Entities
+    ;
+public enum EnrollmentStatus
+{
+    Active = 1,
+    Cancelled = 2
+}
+
+public class Enrollment
+{
+    public int Id { get; private set; }
+
+    public int StudentId { get; private set; }
+    public Student Student { get; private set; } = null!;
+
+    public int CourseInstanceId { get; private set; }
+    public CourseInstance CourseInstance { get; private set; } = null!;
+
+    public DateTime EnrolledAtUtc { get; private set; }
+    public EnrollmentStatus Status { get; private set; }
+
+    private Enrollment() { }
+
+    public Enrollment(int studentId, int courseInstanceId)
+    {
+        StudentId = studentId;
+        CourseInstanceId = courseInstanceId;
+        EnrolledAtUtc = DateTime.UtcNow;
+        Status = EnrollmentStatus.Active;
+    }
+
+    public void Cancel() => Status = EnrollmentStatus.Cancelled;
+}
