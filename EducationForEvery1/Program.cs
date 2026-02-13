68a3094c14185b7e336ab3031ad699736bdedc1e
JoachimPersson88 <joachim.persson88@gmail.com>
2026-02-13 20:19:30 +0100

    "Infrastructure START" Efter att ha bråkat länge med projekt referens, så fick jag äntligen rätt på strukturen med hjälp av chatGPT.

--- EducationForEvery1/Program.cs
+++ EducationForEvery1/Program.cs
@@ -1,6 +1,14 @@
+using EducationForEvery1.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<EducationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EducationDb")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
 app.Run();
+
+public partial class Program { }
