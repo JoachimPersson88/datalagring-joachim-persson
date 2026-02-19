b9bec437a2a1da3c53c46b6ddde918f98e07313b
JoachimPersson88 <joachim.persson88@gmail.com>
2026-02-19 18:55:17 +0100

    "Tests START" Har skapat en "test-host" som byter databasen till InMemory.

---
+++ EducationForEvery1.Tests/TestApplication.cs
@@ -0,0 +1,28 @@
+﻿using EducationForEvery1.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace EducationForEvery1.Tests;
+
+public class TestApplication : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            // Ta bort riktig DbContext-registrering (SQL Server)
+            var descriptor = services.SingleOrDefault(d =>
+                d.ServiceType == typeof(DbContextOptions<EducationDbContext>));
+
+            if (descriptor is not null)
+                services.Remove(descriptor);
+
+            // Lägg till InMemory DB istället
+            services.AddDbContext<EducationDbContext>(options =>
+                options.UseInMemoryDatabase("TestDb"));
+        });
+    }
+}
